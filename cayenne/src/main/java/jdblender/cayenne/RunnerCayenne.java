d9076b6373f4dd31d5038b19ec2969074d303d31
Ruslan Ibragimov <ruslan@ibragimov.by>
2016-11-16 18:45:48 +0300

    Update test cases.

--- cayenne/src/main/java/jdblender/cayenne/RunnerCayenne.java
+++ cayenne/src/main/java/jdblender/cayenne/RunnerCayenne.java
@@ -1,7 +1,5 @@
 package jdblender.cayenne;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import jdblender.cayenne.model.Brands;
 import jdblender.cayenne.model.Models;
 import jdblender.cayenne.model.Series;
@@ -11,24 +9,16 @@ import jdblender.core.FrameworkRunner;
 import jdblender.core.domain.*;
 import org.apache.cayenne.ObjectContext;
 import org.apache.cayenne.configuration.server.ServerRuntime;
-import org.apache.cayenne.query.ObjectSelect;
+import org.apache.cayenne.query.*;
+
 import java.util.Collection;
-import java.util.List;
-import jdblender.cayenne.model.SpareToModel;
-import org.apache.cayenne.exp.Expression;
-import org.apache.cayenne.exp.ExpressionFactory;
-import org.apache.cayenne.query.EJBQLQuery;
-import org.apache.cayenne.query.SQLTemplate;
-import org.apache.cayenne.query.SelectQuery;
 
 /**
  *
  */
 public class RunnerCayenne implements FrameworkRunner {
 
-    private ObjectContext readContext;
-
-    private ObjectContext writeContext;
+    private ObjectContext context;
 
     private ServerRuntime cayenneRuntime;
 
@@ -40,8 +30,7 @@ public class RunnerCayenne implements FrameworkRunner {
     @Override
     public void init(DbConnection connection) {
         cayenneRuntime = new ServerRuntime("cayenne-project.xml");
-        readContext = cayenneRuntime.newContext();
-        writeContext = readContext;
+        context = cayenneRuntime.newContext();
     }
 
     @Override
@@ -51,149 +40,131 @@ public class RunnerCayenne implements FrameworkRunner {
 
     @Override
     public void createBrand(long id, String name) {
-        Brands brands = writeContext.newObject(Brands.class);
+        Brands brands = context.newObject(Brands.class);
         brands.setId(id);
         brands.setName(name);
-        writeContext.commitChanges();
+        context.commitChanges();
     }
 
+    @Override
     public Brand getBrand(long id) {
-        return ObjectSelect
-            .query(Brands.class)
-            .where(Brands.ID.eq(id))
-            .selectOne(readContext);
+        return SelectById
+            .query(Brands.class, id)
+            .selectOne(context);
     }
 
     @Override
     public void createSeries(long id, long brandId, String name) throws Exception {
-        Series series = writeContext.newObject(Series.class);
+        Series series = context.newObject(Series.class);
         series.setId(id);
         series.setName(name);
-        series.setBrandId(brandId);
-        series.setBrand(ObjectSelect
-            .query(Brands.class)
-            .where(Brands.ID.eq(brandId))
-            .selectOne(readContext));
-        writeContext.commitChanges();
+        series.setBrand(SelectById
+            .query(Brands.class, id)
+            .selectOne(context));
+        context.commitChanges();
     }
 
     @Override
     public jdblender.core.domain.Series getSeries(long id) throws Exception {
-        return ObjectSelect
-            .query(Series.class)
-            .where(Series.ID.eq(id))
-            .selectOne(readContext);
+        return SelectById
+            .query(Series.class, id)
+            .selectOne(context);
     }
 
     @Override
     public SeriesObj getSeriesObj(long id) throws Exception {
-        return ObjectSelect
-            .query(Series.class)
+        return SelectById
+            .query(Series.class, id)
             .prefetch(Series.BRAND.joint())
-            .where(Series.ID.eq(id))
-            .selectOne(readContext);
+            .selectOne(context);
     }
 
     @Override
     public void createModel(long id, long seriesId, String name) {
-        Models models = writeContext.newObject(Models.class);
+        Models models = context.newObject(Models.class);
         models.setId(id);
-        models.setSeriesId(seriesId);
+        models.setSeries(SelectById
+            .query(Series.class, seriesId)
+            .selectOne(context));
         models.setName(name);
-        writeContext.commitChanges();
+        context.commitChanges();
     }
 
     @Override
     public Model getModel(long id) {
-        return ObjectSelect
-            .query(Models.class)
-            .where(Models.ID.eq(id))
-            .selectOne(readContext);
+        return SelectById
+            .query(Models.class, id)
+            .selectOne(context);
     }
 
     @Override
     public ModelObj getModelObj(long id) {
-        return ObjectSelect
-            .query(Models.class)
+        return SelectById
+            .query(Models.class, id)
             .prefetch(Models.SERIES.joint())
-            .where(Models.ID.eq(id))
-            .selectOne(readContext);
+            .selectOne(context);
     }
 
     @Override
     public void createSpare(long id, long brandId, String name, String label, boolean flag, int num) {
-        Spares spares = writeContext.newObject(Spares.class);
+        Spares spares = context.newObject(Spares.class);
         spares.setId(id);
         spares.setName(name);
         spares.setLabel(label);
         spares.setFlag(flag);
         spares.setNum(num);
-        spares.setBrandId(brandId);
-        spares.setBrand(ObjectSelect
-            .query(Brands.class)
-            .where(Brands.ID.eq(brandId))
-            .selectOne(readContext));
-        writeContext.commitChanges();
+        spares.setBrand(SelectById
+            .query(Brands.class, brandId)
+            .selectOne(context));
+        context.commitChanges();
     }
 
     @Override
     public Spare getSpare(long id) throws Exception {
-        return ObjectSelect
-            .query(Spares.class)
-            .where(Spares.ID.eq(id))
-            .selectOne(readContext);
+        return SelectById
+            .query(Spares.class, id)
+            .selectOne(context);
     }
 
     @Override
     public SpareObj getSpareObj(long id) throws Exception {
-        return ObjectSelect
-            .query(Spares.class)
+        return SelectById
+            .query(Spares.class, id)
             .prefetch(Spares.BRAND.joint())
-            .where(Spares.ID.eq(id))
-            .selectOne(readContext);
+            .selectOne(context);
     }
 
     @Override
     public void linkModel2Spare(long modelId, long spareId) {
-        SpareToModel s2m = writeContext.newObject(SpareToModel.class);
-        s2m.setModelId(modelId);
-        s2m.setSpareId(spareId);
-        writeContext.commitChanges();
+        Models models = SelectById
+            .query(Models.class, modelId)
+            .selectOne(context);
+
+        Spares spares = SelectById
+            .query(Spares.class, spareId)
+            .selectOne(context);
+
+        spares.addToModels(models);
+
+        context.commitChanges();
     }
 
     @Override
     public void linkModel2SpareOptimized(long modelId, long spareId) throws Exception {
-        // FIXME have no Idea how call native query here
-        SpareToModel s2m = writeContext.newObject(SpareToModel.class);
-        s2m.setModelId(modelId);
-        s2m.setSpareId(spareId);
-        writeContext.commitChanges();
+        SQLExec
+            .query("INSERT INTO spare_to_model (spare_id, model_id) VALUES (#bind($spareId), #bind($modelId))")
+            .paramsArray(spareId, modelId)
+            .execute(context);
     }
 
     @Override
     public ModelObj getModelObjWithSpares(long id) throws Exception {
-        Models model = ObjectSelect
+        return ObjectSelect
             .query(Models.class)
             .prefetch(Models.SERIES.joint())
+            .prefetch(Models.SPARES.joint())
             .where(Models.ID.eq(id))
-            .selectOne(readContext);
-
-        // This implementation is also sucks but I do not know how to do it in other way
-        ArrayList<Long> sIds = new ArrayList<>();
-        ObjectSelect
-            .query(SpareToModel.class)
-            .where(SpareToModel.MODEL_ID.eq(id))
-            .select(readContext)
-            .stream().forEach(it -> sIds.add(it.getSpareId()));
-
-        List<Spares> spares = ObjectSelect
-            .query(Spares.class)
-            .prefetch(Spares.BRAND.joint())
-            .where(Spares.ID.in(sIds))
-            .select(readContext);
-
-        model.setSpares(spares);
-        return model;
+            .selectOne(context);
     }
 
     @Override
@@ -216,6 +187,6 @@ public class RunnerCayenne implements FrameworkRunner {
             }
         }
 
-        return (Collection)query.select(readContext);
+        return (Collection) query.select(context);
     }
 }
